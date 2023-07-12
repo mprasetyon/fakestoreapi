3f2a6265657e5dcba6056e9e2296f63f51f70c3a
Muhammad Prasetyo <prasetyo.nugroho@raywhite.co.id>
2023-07-12 10:29:04 +0700

    Product Context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
 
-const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-};
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom' ;
 
+import Home from "./pages/Home";
+import ProductDetails from './pages/ProductDetails';
+
+import Sidebar from './components/Sidebar';
+import Header from './components/Header' ;
+import Footer from './components/Footer';
+
+const App = () => {
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+        <Sidebar/>
+        <Footer/>
+      </Router>
+    </div>
+  );
+};
 export default App;
--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React,{ createContext,useEffect,useState} from 'react';
 
-const ProductContext = () => {
-  return <div>ProductContext</div>;
-};
+export const ProductContext = createContext();
 
-export default ProductContext;
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+  
+  const fetchProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    setProducts(data);
+    console.log(data);
+  };
+  fetchProducts();
+  },[]);
+  return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
+}
+export default ProductProvider;
